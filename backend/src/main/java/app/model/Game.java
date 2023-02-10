2ee92cb588979b53dd965410d4292d2936db1205
BraisCabo <braiscabofelpete@gmail.com>
2023-02-10 10:36:27 +0100

    Game, Purchase, Review and User models created

--- backend/src/main/java/app/model/Game.java
+++ backend/src/main/java/app/model/Game.java
@@ -1,9 +1,18 @@
 package app.model;
 
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Game {
@@ -11,45 +20,111 @@ public class Game {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	
-	private String title;
+	private String name;
+	@Column(length = 50000)
 	private String description;
-	private String os;
-	
+	private float price;
+	private String category;
+
+	@Lob
+	@JsonIgnore
+	private Blob titleImage;
+
+	@Lob
+	@JsonIgnore
+	private List<Blob> gameplayImages =  new ArrayList<>();
+
+	@OneToMany
+	private List<Review> reviews = new ArrayList<>();
+
+	@Column(length = 50000)
+	private String minimumRequirements;
+
 	public Game() {
-		
+
 	}
-	
-	public Game(String title, String description, String os) {
-		super();
-		this.title = title;
+
+	public Game(String name, String description, float price, String category, Blob titleImage, List<Blob> gameplayImages, String minimumRequirements) {
+		this.name = name;
 		this.description = description;
-		this.os = os;
+		this.price = price;
+		this.category = category;
+		this.titleImage = titleImage;
+		this.gameplayImages = gameplayImages;
+		this.minimumRequirements = minimumRequirements;
 	}
-	
+
+
 	public long getId() {
-		return id;
+		return this.id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	public String getTitle() {
-		return title;
+
+	public String getName() {
+		return this.name;
 	}
-	public void setTitle(String title) {
-		this.title = title;
+
+	public void setName(String name) {
+		this.name = name;
 	}
+
 	public String getDescription() {
-		return description;
+		return this.description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public String getOs() {
-		return os;
+
+	public float getPrice() {
+		return this.price;
 	}
-	public void setOs(String os) {
-		this.os = os;
+
+	public void setPrice(float price) {
+		this.price = price;
 	}
-	
-	
+
+	public String getCategory() {
+		return this.category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Blob getTitleImage() {
+		return this.titleImage;
+	}
+
+	public void setTitleImage(Blob titleImage) {
+		this.titleImage = titleImage;
+	}
+
+	public List<Blob> getGameplayImages() {
+		return this.gameplayImages;
+	}
+
+	public void setGameplayImages(List<Blob> gameplayImages) {
+		this.gameplayImages = gameplayImages;
+	}
+
+	public List<Review> getReviews() {
+		return this.reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	public String getMinimumRequirements() {
+		return this.minimumRequirements;
+	}
+
+	public void setMinimumRequirements(String minimumRequirements) {
+		this.minimumRequirements = minimumRequirements;
+	}
+
 }
---
+++ backend/src/main/java/app/model/Review.java
@@ -0,0 +1,92 @@
+package app.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Review {
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Game game;
+
+    private LocalDate date = LocalDate.now();
+
+    private int rating;
+
+    @Column(length = 50000)
+    private String comment;
+
+    public Review() {
+
+    }
+
+    public Review(User user, Game game, int rating, String comment) {
+        super();
+        this.user = user;
+        this.game = game;
+        this.rating = rating;
+        this.comment = comment;
+    }
+
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Game getGame() {
+        return this.game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public int getRating() {
+        return this.rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
