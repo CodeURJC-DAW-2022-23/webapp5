726084764f1b83f61d5fd290577df2e14a3448e3
JavierGasparinMunoz <j.gasparino.2020@alumnos.urjc.es>
2023-02-17 18:12:29 +0100

    checkout-correo-v2

--- backend/src/main/java/app/controller/CheckoutController.java
+++ backend/src/main/java/app/controller/CheckoutController.java
@@ -5,11 +5,13 @@ import java.security.Principal;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.Optional;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -57,25 +59,42 @@ public class CheckoutController {
 		}
 	}
     @GetMapping("/{userId}/checkout")
-    public String checkout(Model model,@PathVariable long userId) {
+    public String cartCheckout(Model model,@PathVariable long userId) {
         try{
             User user = userService.findById(userId).orElseThrow();
             if (!user.getId().equals(currentUser.getId())){
                 throw new Exception();
             } 
-            currentUser.getCart();
-            userService.save(currentUser);
-            return "redirect:/";
+            if (currentUser.getCart().isEmpty()){
+                throw new Exception();
+            } else if (currentUser.getBillingInformation() != null){
+                return "checkout";
+            } else {
+                userService.save(currentUser);
+                return "redirect:/";
+            }
+
+            
         } catch(Exception e){
             return "redirect:/error";
         }
 	}
 
-    @RequestMapping("/{userId}/checkout")
-
-    private void getDataCheckout(User user){
-        if(user.getBillingInformation().equals(null)){
-
+    @PostMapping("/{userId}/checkout")
+    public String checkoutProcess(Model model,@PathVariable long userId,String billing_firstname,String billing_lastname,String billing_street,String billing_apartment,String billing_city,String billing_country,String billing_postcode,String billing_email,String billing_phone,String billing_aditionalinfo) {
+        try{
+            User user = userService.findById(userId).orElseThrow();
+            if (!user.getId().equals(currentUser.getId())){
+                throw new Exception();
+            } 
+            String billingInfo = billing_firstname + billing_lastname + billing_street + 
+            billing_apartment + billing_city + billing_country + billing_postcode + 
+            billing_email + billing_phone + billing_aditionalinfo;
+            user.setBillingInformation(billingInfo);
+            return "redirect:/";
+        } catch(Exception e){
+            return "redirect:/error";
         }
-    }
+			
+	}
 }
