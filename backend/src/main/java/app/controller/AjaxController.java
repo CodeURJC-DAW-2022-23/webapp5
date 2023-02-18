ebcc9288cafdddc99e32e76c2742c9554ed9e220
Brais <braiscabofelpete@gmail.com>
2023-02-18 16:52:04 +0100

    Revert "load in the checkout done"

--- backend/src/main/java/app/controller/AjaxController.java
+++ backend/src/main/java/app/controller/AjaxController.java
@@ -1,22 +1,17 @@
 package app.controller;
 
-import java.security.Principal;
-
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import app.model.Game;
 import app.service.GameService;
 import app.service.ReviewService;
-import app.service.UserService;
-import app.model.User;
 
 @Controller
 public class AjaxController {
@@ -26,26 +21,6 @@ public class AjaxController {
 
     @Autowired
     private ReviewService reviewService;
-
-	@Autowired
-	private UserService userService;
-
-	User currentUser;
-
-	@ModelAttribute
-	public void addAttributes(Model model, HttpServletRequest request) {
-
-		Principal principal = request.getUserPrincipal();
-
-		if(principal != null) {
-			userService.findByMail(principal.getName()).ifPresent(u -> currentUser = u);
-			model.addAttribute("logged", true);
-			model.addAttribute("currentUser", currentUser);
-			model.addAttribute("admin", request.isUserInRole("ADMIN"));
-		} else {
-			model.addAttribute("logged", false);
-		}
-	}
 
     @GetMapping("/moreReviews/{id}/{page}")
 	public String getMoreReviews(Model model, @PathVariable int page, @PathVariable long id) {
@@ -68,7 +43,8 @@ public class AjaxController {
 			category = null;
 		}
 		if (page <= (int) Math.ceil(gameService.countByCategoryAndName(name, category)/6)) {
-			model.addAttribute("foundGames", gameService.findByCategoryAndName(name, category, PageRequest.of(page,6)));
+            List<Game> findByGame = gameService.findByCategoryAndName(name, category, PageRequest.of(page,6));
+			model.addAttribute("foundGames", findByGame);
 			return "moreGames";
 		}
 		return null;
@@ -78,22 +54,9 @@ public class AjaxController {
 	public String getMoreIndexGames(Model model, @PathVariable int page) {
 		// Before returning a page it confirms that there are more left
 		if (page <= (int) Math.ceil(gameService.countGames()/6)) {
-			model.addAttribute("foundGames", gameService.findGames(PageRequest.of(page,6)));
+            List<Game> findByGame = gameService.findGames(PageRequest.of(page,6));
+			model.addAttribute("foundGames", findByGame);
 			return "indexGames";
-		}
-		return null;
-	}
-
-	@GetMapping("/moreCartGames/{id}/{page}")
-	public String getMoreCartGames(Model model, @PathVariable int page, @PathVariable long id) {
-		// Before returning a page it confirms that there are more left
-		User user = userService.findById(id).orElseThrow();
-		if (!user.getId().equals(currentUser.getId())){
-			return null;
-		}
-		if (page <= (int) Math.ceil(userService.countGamesInCartByUserId(id)/3)) {
-			model.addAttribute("foundGames", userService.findGamesInCartByUserId(id, PageRequest.of(page,3)));
-			return "cartGames";
 		}
 		return null;
 	}
--- backend/src/main/java/app/controller/IndexController.java
+++ backend/src/main/java/app/controller/IndexController.java
@@ -36,8 +36,6 @@ public class IndexController {
 			model.addAttribute("currentUser", currentUser);
 			model.addAttribute("emptyCart", currentUser.getCart().isEmpty());
 			model.addAttribute("admin", request.isUserInRole("ADMIN"));
-			model.addAttribute("userCart", userService.findGamesInCartByUserId(currentUser.getId(), PageRequest.of(0,3)));
-			model.addAttribute("moreGamesInCart", currentUser.getCart().size() > 3);
 		} else {
 			model.addAttribute("logged", false);
 		}
@@ -47,10 +45,5 @@ public class IndexController {
 	public String showBooks(Model model) {
 		model.addAttribute("allGames", gameService.findGames(PageRequest.of(0,6)));
 		return "index";
-	}
-
-	@GetMapping("/checkout/{id}")
-	public String checkout(Model model, @PathVariable long id) {
-		return "checkout";
 	}
 }
