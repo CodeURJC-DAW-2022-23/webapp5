f9dc524b2f87c5a11a96ba59bc463e27460b18c5
JavierGasparinMunoz <j.gasparino.2020@alumnos.urjc.es>
2023-02-19 16:57:35 +0100

    Correo-v1

---
+++ backend/src/main/java/app/Email/EmailServiceImpl.java
@@ -0,0 +1,49 @@
+package app.Email;
+
+// Importing required classes
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+// Annotation
+@Service
+// Class
+// Implementing EmailService interface
+public class EmailServiceImpl{
+
+    @Autowired private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}") private String sender;
+
+    // Method 1
+    // To send a simple email
+    public String sendSimpleMail(EmailDetails details)
+    {
+
+        // Try block to check for exceptions
+        try {
+
+            // Creating a simple mail message
+            SimpleMailMessage mailMessage
+                    = new SimpleMailMessage();
+
+            // Setting up necessary details
+            mailMessage.setFrom(sender);
+            mailMessage.setTo(details.getRecipient());
+            mailMessage.setText(details.getMsgBody());
+            mailMessage.setSubject(details.getSubject());
+
+            // Sending the mail
+            javaMailSender.send(mailMessage);
+            return "Mail Sent Successfully...";
+        }
+
+        // Catch block to handle the exceptions
+        catch (Exception e) {
+            return "Error while Sending Mail";
+        }
+    }
+
+}
