bd1bc73f2cd3578195e37d10a82c6a3108473764
BraisCabo <braiscabofelpete@gmail.com>
2023-02-28 10:08:33 +0100

    anotation fix

--- backend/src/main/java/app/repository/UserRepository.java
+++ backend/src/main/java/app/repository/UserRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.jpa.repository.Modifying;
 
 import app.model.Game;
 import app.model.User;
