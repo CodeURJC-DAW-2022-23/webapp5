f39d64a95428a5ecc4a9d8d25b1d5b3058fa5611
BraisCabo <braiscabofelpete@gmail.com>
2023-03-15 16:17:38 +0100

    title changed

--- code/frontend/src/app/app.component.ts
+++ code/frontend/src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  title = 'KeyWhale';
+  constructor(private titleService:Title) {
+  }
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+  }
 }
